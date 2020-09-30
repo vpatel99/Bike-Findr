42cf21b19080dd0c0f7558d11792007c684328df
Vivek Patel <vivekpatel1999.vdp@gmail.com>
2020-09-30 16:24:13 -0400

    Skeleton code

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+// this will be the main js file that will render mostly all the components
+
+function App() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App
